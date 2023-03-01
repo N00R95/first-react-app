bca9de8da4f06eca9dc01b5a3bff23376f8cebcf
Nour <70517708+N00R95@users.noreply.github.com>
2023-03-02 01:12:23 +0300

    second commit

---
+++ firstapp/src/Components/Footer.js
@@ -0,0 +1,10 @@
+function Footer(props) {
+    return (
+        <div className="copyright">
+            <p> {props.name}
+                Made with love by Myself</p>
+        </div>
+    );
+};
+
+export default Footer;
